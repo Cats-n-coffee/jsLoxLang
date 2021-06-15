6c59a8bc918d04ae9c3dfe160b9b2e8d6b5d3f80
Cats-n-coffee <lucie.macaigne@gmail.com>
2021-06-15 16:07:03 -0400

    added runPrompt

--- README.md
+++ README.md
@@ -4,4 +4,10 @@ This is my version of the Lox language from Crafting Interpreters. My version is
 
 ## Scanner / Lexer
 
-Takes the code and splits it into tokens, that will then be given to the parser. It reads strings raw source code and does its job of creating tokens.
+Takes the code and splits it into tokens, that will then be given to the parser. 
+It reads strings raw source code and does its job of creating tokens.
+- Main method: "directs" the given input: uses the array length to return an error, read or run the prompt.
+- runFile method: read the file, transforms the bytes to a string. Needed if we read code that's already written?
+- runPrompt method: takes input from standard input, transforms it to a buffer. Needed when we're typing?
+  In a for loop, we print each line or break out of the loop if there is no line.
+- run method: scans each line to create tokens.
--- jsLoxLang.js
+++ jsLoxLang.js
@@ -3,20 +3,27 @@ This is the main file (entry file also) for JsLoxLang.
 The main() function expects a string inside an array, 
 which should be the raw input given by the user 
 */
+const fs = require('fs');
+const readline = require('readline');
 
-class Lox {
+class JsLox {
     constructor(rawCode) {
         this.rawCode = rawCode;
     }
 // Do extra check to ensure only a string is contained in the array?
     main() {
         if (this.rawCode.length > 1) {
-            console.error('Input array is too long, exiting program')
-            process.exit(0);
+            console.error('Input array is too long')
+            process.on('error', () => {
+                console.log('Exiting process')
+                process.exit(0);
+            })
+            process.emit('error')
         }
         else if (this.rawCode.length === 1) {
             console.log('good length, let\'s read', this.rawCode);
             this.runFile(this.rawCode[0]);
+            // In the Java example Charset.defaultCharset() returns the charset for the machine (OS) running the program
         }
         else {
             console.log('let\s run a prompt with this', this.rawCode);
@@ -24,17 +31,40 @@ class Lox {
         }
     }
 
-    static runFile(givenArg) {
+    runFile(givenArg) {
         console.log('inside the runfile function', givenArg)
+        const fileContents = fs.readFileSync(givenArg, 'utf-8');
+        console.log('reading the file', fileContents)
     }
 
-    static runPrompt() {
-        console.log('inside the runprompt function')
-    }
+    runPrompt() {
+        console.log('inside the runprompt function');
 
-    //Need an InputStreamReader class
-    // Need an BufferReader class
+        let inputStr = '';
+        
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+
+        rl.setPrompt("> ");
+        rl.prompt()
+        rl.on("line", userInput => {
+            inputStr += userInput;
+            process.stdout.write(inputStr)
+        })
+        
+        rl.on("close", () => {
+            console.log('\n that string is', inputStr)
+            console.log('exiting process')
+            process.exit(0)
+        })
+        
+    }
 }
 
-const jsLoxInstance = new Lox(['x = x + 1']);
+const jsLoxInstance = new JsLox([]);
 jsLoxInstance.main();
+
+// https://stackoverflow.com/questions/61394928/get-user-input-through-node-js-console
+// https://stackoverflow.com/questions/17837147/user-input-in-node-js
